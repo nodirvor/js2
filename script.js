b1d3e5dfefef0d816797613846fef6baf0f415fd
nodir <nodirganiyev59@gmail.com>
2022-11-19 19:12:32 +0500

    js2

---
+++ script.js
@@ -0,0 +1,119 @@
+var model = prompt("ismingizni kiriting")
+console.log("sizning ismingiz: "+model)
+
+
+
+var model1 = prompt("familyangizni kiriting")
+console.log("sizning familyangiz: "+model1)
+
+var model2 = +prompt("yoshingizni kiriting")
+console.log("yoshingiz: "+model2)
+
+ var misol00 = +prompt("2+2=?")
+ if (misol00 ==2){
+    alert("javobingiz tog'ri")
+    
+ }
+ else if(misol00 >=2){
+    alert("no tog'ri tog'ri javob : 2")
+  
+ }
+ else if(misol00 ){
+    alert("no tog'ri tog'ri javob : 2")
+    
+ }
+
+
+
+
+
+
+
+ var misol00 = +prompt("2+25=?")
+ if (misol00 ==27){
+    alert("javobingiz tog'ri")
+    
+ }
+ else if(misol00 >=27){
+    alert("no tog'ri tog'ri javob : 27")
+  
+ }
+ else if(misol00 <=27){
+    alert("no tog'ri tog'ri javob : 27")
+    
+ }
+
+ var misol1 = +prompt("12-2=?")
+ if (misol00 ==10){
+    alert("javobingiz tog'ri")
+    
+ }
+ else if(misol00 >=9){
+    alert("no tog'ri tog'ri javob : 10")
+  
+ }
+ else if(misol00 <=10 ){
+    alert("no tog'ri tog'ri javob : 10")
+    
+ }
+
+ var misol00 = +prompt("2*2=?")
+ if (misol00 ==4){
+    alert("javobingiz tog'ri")
+    
+ }
+ else if(misol00 >=4){
+    alert("no tog'ri tog'ri javob : 4")
+  
+ }
+ else if(misol00 <=4 ){
+    alert("no tog'ri tog'ri javob : 4")
+    
+ }
+
+ var misol00 = +prompt("5*5=?")
+ if (misol00 ==25){
+    alert("javobingiz tog'ri")
+    
+ }
+ else if(misol00 >=25){
+    alert("no tog'ri tog'ri javob : 25")
+  
+ }
+ else if(misol00 <=25){
+    alert("no tog'ri tog'ri javob : 25")
+    
+ }
+
+//   var misol1 = +prompt("12-2=?")
+ // console.log(" misol 1 : tori javob 12-2 = "+(12-2)+"  sizning javob "+misol1)
+ 
+ // var misol2 = +prompt("12*2=?")
+ // console.log(" misol 1 : tori javob 12*2 = "+(12*2)+"  sizning javob "+misol2)
+ 
+ 
+ 
+ // var misol3 = +prompt("12/2=?")
+ // console.log(" misol 1 : tori javob 12/2 = "+(12/2)+"  sizning javob "+misol3)
+ 
+ 
+ 
+ // var misol4 = +prompt("122*2=?")
+ // console.log(" misol 1 : tori javob 122*2 = "+(122*2)+"  sizning javob "+misol4)
+ 
+ 
+ // var misol5 = +prompt("1222111*2=?")
+ // console.log(" misol 1 : tori javob 1222111*2 = "+(1222111*2)+"  sizning javob "+misol5)
+ 
+ 
+ // var misol6 = +prompt("2*2=?")
+ // console.log(" misol 1 : tori javob 2*2 = "+(2*2)+"  sizning javob "+misol6)
+ 
+ // var a = +prompt("siz istalgan soni kriting juf yoki toq son ekanligini aniqlab beramiz")
+ 
+ // if(a = 2% + 0 ){
+ //     alert("toq")}
+ //     else if (a = 2% + 1){
+ //     alert("juft")
+ //     }
+ 
